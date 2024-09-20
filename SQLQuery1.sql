a3d15467f8a0329b53f833efc07e426d3d978c3d
mahnoorbabar105 <135722286+mahnoorbabar105@users.noreply.github.com>
2024-09-20 14:35:14 +0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[JobData] (
+    [Id]                INT            IDENTITY (1, 1) NOT NULL,
+    [TitleCompany]      NVARCHAR(255)  NOT NULL,   
+    [ClosingType]       NVARCHAR(255)  NOT NULL, 
+    [InternalReference] NVARCHAR(MAX)  NOT NULL,
+    [KBARequired]       BIT            NOT NULL, 
+    [PropertyAddress1]  NVARCHAR(MAX)  NOT NULL,
+    [PropertyAddress2]  NVARCHAR(MAX)  NOT NULL,
+    [PropertyCity]      NVARCHAR(MAX)  NOT NULL,
+    [PropertyState]     NVARCHAR(MAX)  NOT NULL,
+    [PropertyZipCode]   NVARCHAR(MAX)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
